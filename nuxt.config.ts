9ee97b7f25809852f2b930adbc030b663700ca78
John Farrar <johnfarrar@sosapps.com>
2024-10-24 10:59:13 -0400

    early init

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxt/test-utils',
+    '@nuxt/icon',
+    '@nuxt/scripts'
+  ]
+})
