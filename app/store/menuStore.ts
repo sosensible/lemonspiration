2046635d51f1e98e4e7ab8a8288093f70423986c
John Farrar <johnfarrar@sosapps.com>
2024-10-28 18:56:40 -0400

    updated the project for ynab integratoin

--- app/store/menuStore.ts
+++ app/store/menuStore.ts
@@ -2,24 +2,41 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
+interface MenuItem {
+  id: number;
+  name: string;
+  link: string;
+  meta: {
+    active: boolean;
+  };
+  children?: MenuItem[]; // Optional nested array of MenuItem
+}
+
 export const useMenuStore = defineStore('menu', () => {
-  const menus = ref([
-    { id: 1, name: 'Home', link: '/' },
-    { id: 2, name: 'About', link: '/about' },
-    { id: 3, name: 'Contact', link: '/contact' },
+  const menus = ref<MenuItem[]>([
+    { id: 1, name: 'Home', link: '/', meta: { active: true } },
+    { id: 2, name: 'About', link: '/about', meta: { active: false } },
+    { id: 3, name: 'Contact', link: '/contact', meta: { active: false } },
   ]);
 
-  const addMenu = (menu) => {
+  const activeMenus = computed(() => menus.value.filter(menu => menu.meta.active));
+
+  const addMenu = (menu: MenuItem) => {
     menus.value.push(menu);
   };
 
-  const removeMenu = (id) => {
+  const removeMenu = (id: number) => {
     menus.value = menus.value.filter(menu => menu.id !== id);
   };
 
   return {
+    activeMenus,
     menus,
     addMenu,
     removeMenu,
   };
 });
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useMenuStore, import.meta.hot));
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import fs from 'fs';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
 
@@ -14,5 +16,24 @@ export default defineNuxtConfig({
     '@nuxt/test-utils',
     '@nuxt/icon',
     '@nuxt/scripts'
-  ]
+  ],
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE || 'http://localhost:3000/api',
+      ynab: {
+        apiKey: process.env.YNAB_ACCESS_TOKEN || '',
+        clientId: process.env.YNAB_CLIENT_ID || '',
+        clientSecret: process.env.YNAB_CLIENT_SECRET || '',
+        redirectUri: process.env.YNAB_REDIRECT_URI || '',
+      }
+    },
+    
+    devServer: {
+      https: {
+        key: fs.readFileSync('server.key'),
+        cert: fs.readFileSync('server.cert')
+      }
+    },
+  },
 })
